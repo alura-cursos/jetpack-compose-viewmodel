f95d1c19e9dc1db348de6501ecefff8b3205f65d
Alex Felipe <alexfelipevieira@gmail.com>
2022-11-03 15:39:02 -0300

    2.2

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +40,7 @@ fun HomeScreen(products: List<Product>) {
         "Doces" to sampleCandies,
         "Bebidas" to sampleDrinks
     )
-    var text by remember {
+    var text by rememberSaveable {
         mutableStateOf("")
     }
 
