22743e03c91e15b3c8c753d71869d28b6c986c67
Alex Felipe <alexfelipevieira@gmail.com>
2022-11-04 11:29:20 -0300

    4.3

--- app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -4,12 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.aluvery.dao.ProductDao
 import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.sampleCandies
 import br.com.alura.aluvery.sampledata.sampleDrinks
 import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.ui.states.HomeScreenUiState
+import kotlinx.coroutines.launch
 
 class HomeScreenViewModel : ViewModel() {
 
@@ -17,12 +19,6 @@ class HomeScreenViewModel : ViewModel() {
 
     var uiState: HomeScreenUiState by mutableStateOf(
         HomeScreenUiState(
-            sections = mapOf(
-                "Todos produtos" to dao.products(),
-                "Promoções" to sampleDrinks + sampleCandies,
-                "Doces" to sampleCandies,
-                "Bebidas" to sampleDrinks
-            ),
             onSearchChange = {
                 uiState = uiState.copy(
                     searchText = it,
@@ -31,6 +27,21 @@ class HomeScreenViewModel : ViewModel() {
             }
         ))
         private set
+
+    init {
+        viewModelScope.launch {
+            dao.products().collect { products ->
+                uiState = uiState.copy(
+                    sections = mapOf(
+                        "Todos produtos" to products,
+                        "Promoções" to sampleDrinks + sampleCandies,
+                        "Doces" to sampleCandies,
+                        "Bebidas" to sampleDrinks
+                    )
+                )
+            }
+        }
+    }
 
     private fun containsInNameOrDescription(text: String) = { product: Product ->
         product.name.contains(
@@ -45,7 +56,7 @@ class HomeScreenViewModel : ViewModel() {
     private fun searchedProducts(text: String): List<Product> =
         if (text.isNotBlank()) {
             sampleProducts.filter(containsInNameOrDescription(text)) +
-                    dao.products().filter(containsInNameOrDescription(text))
+                    dao.products().value.filter(containsInNameOrDescription(text))
         } else emptyList()
 
 }
