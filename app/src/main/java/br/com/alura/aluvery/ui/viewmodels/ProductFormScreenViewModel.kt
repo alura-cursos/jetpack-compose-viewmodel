ff1c86d516fc1c0c450cb26ea58451e4136035b0
Alex Felipe <alexfelipevieira@gmail.com>
2022-11-07 09:50:50 -0300

    5.3

--- app/src/main/java/br/com/alura/aluvery/ui/viewmodels/ProductFormScreenViewModel.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/viewmodels/ProductFormScreenViewModel.kt
@@ -2,6 +2,8 @@ package br.com.alura.aluvery.ui.viewmodels
 
 import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
+import br.com.alura.aluvery.dao.ProductDao
+import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.ui.states.ProductFormUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,49 +13,68 @@ import java.text.DecimalFormat
 
 class ProductFormScreenViewModel : ViewModel() {
 
+
+    private val dao = ProductDao()
     private val _uiState: MutableStateFlow<ProductFormUiState> = MutableStateFlow(
-            ProductFormUiState()
+        ProductFormUiState()
     )
     val uiState get() = _uiState.asStateFlow()
 
-    val formatter =
+    private val formatter =
         DecimalFormat("#.##")
 
     init {
         _uiState.update { currentState ->
             currentState.copy(
-                    onUrlChange = {
-                        _uiState.value = _uiState.value.copy(
-                                url = it
-                        )
-                    },
-                    onNameChange = {
-                        _uiState.value = _uiState.value.copy(
-                                name = it
-                        )
-                    },
-                    onPriceChange = {
-                        val price = try {
-                            formatter.format(BigDecimal(it))
-                        } catch (e: IllegalArgumentException) {
-                            if (it.isEmpty()) {
-                                it
-                            } else {
-                                null
-                            }
+                onUrlChange = {
+                    _uiState.value = _uiState.value.copy(
+                        url = it
+                    )
+                },
+                onNameChange = {
+                    _uiState.value = _uiState.value.copy(
+                        name = it
+                    )
+                },
+                onPriceChange = {
+                    val price = try {
+                        formatter.format(BigDecimal(it))
+                    } catch (e: IllegalArgumentException) {
+                        if (it.isEmpty()) {
+                            it
+                        } else {
+                            null
                         }
-                        price?.let {
-                            _uiState.value = _uiState.value.copy(
-                                    price = price
-                            )
-                        }
-                    },
-                    onDescriptionChange = {
+                    }
+                    price?.let {
                         _uiState.value = _uiState.value.copy(
-                                description = it
+                            price = price
                         )
                     }
+                },
+                onDescriptionChange = {
+                    _uiState.value = _uiState.value.copy(
+                        description = it
+                    )
+                }
             )
+        }
+    }
+
+    fun save() {
+        _uiState.value.run {
+            val convertedPrice = try {
+                BigDecimal(price)
+            } catch (e: NumberFormatException) {
+                BigDecimal.ZERO
+            }
+            val product = Product(
+                name = name,
+                image = url,
+                price = convertedPrice,
+                description = description
+            )
+            dao.save(product)
         }
     }
 
